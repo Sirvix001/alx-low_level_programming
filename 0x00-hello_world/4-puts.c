0b3ee43c0244727bdd700a5a04b17289d5acc97e
victor <victoriheduru@gmail.com>
2022-09-08 16:35:43 -0700

    commit for task 5 and 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,2 @@
-#include <stdio.h>
+#!/bin/bash
 
-/**
- * main - Printing
- *
- * Return: Always 0 (successful)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
