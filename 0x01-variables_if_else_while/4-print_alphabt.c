b16c53adf35c864bd518a5fd34a33967f305357c
victor <victoriheduru@gmail.com>
2022-09-09 19:28:58 -0700

    for task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: print alphabet in lowercase
+ * Return 0
+ */
+
+int main(void)
+{
+int letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'e' && letter != 'q')
+{
+putchar(letter);
+}
+else
+{
+}
+}
+putchar('\n');
+return (0);
+}
+
