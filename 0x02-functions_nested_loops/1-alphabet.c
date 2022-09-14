c55708ad15389510fdf8eaa0cdc14379841539d2
Sirvix001 <111112240+Sirvix001@users.noreply.github.com>
2022-09-14 02:46:08 +0100

    Commit for task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet - Entry point
- * Description: a function that prints the alphabet, in lowercase, followed by a new line.
- * Return: void
- */
-void print_alphabet(void);
+* print_alphabet - prints the alphabet, in lowercase, followed by a new line
+*/
+void print_alphabet(void)
 {
-	char letter = 'a';
+char ch;
 
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-		letter++;
-	}
-	_putchar('\n');
+ch = 'a';
+
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
 }
 
+_putchar("\n");
+}
