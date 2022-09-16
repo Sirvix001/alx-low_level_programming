05f97b919a8588ac32a1694024e017582418b984
Sirvix001 <111112240+Sirvix001@users.noreply.github.com>
2022-09-16 01:33:16 +0100

    For task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,23 +10,14 @@ int main(void)
 int i;
 
 printf("Infinite loop incoming :(\n");
-
-i = 0;
-<<<<<<< HEAD
-/**
- *while (i < 10);
- *{
- *putchar(i);
- *}
- */
-=======
-/*
-* while (i < 10);
-* {
-* putchar(i);
-* }
-*/
->>>>>>> 3fb0eef715e65cc37bbfca2569153f53605cf674
+  /**
+   *i = 0;
+   *
+   *while (i < 10)
+   *{
+   *putchar(i);
+   *}
+   */
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
